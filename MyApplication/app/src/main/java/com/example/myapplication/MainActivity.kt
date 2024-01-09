0575e76f3b7a4d76f418793b25e95387bf329ede
btLong402 <111285054+btLong402@users.noreply.github.com>
2024-01-10 04:01:45 +0700

    log out

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,51 @@
 package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import com.example.myapplication.fragments.HomeFragment
+import com.example.myapplication.fragments.SplashFragment
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.logout -> {
+                logoutUser()
+            }
+            // Add other cases for additional menu items if needed
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    private fun logoutUser() {
+        // Log out the user using FirebaseAuth
+        FirebaseAuth.getInstance().signOut()
+
+        // TODO: You may want to navigate the user to the login screen or perform other actions after logout
+
+        // Example: If you are using Navigation component, navigate to the login fragment
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.navigate(R.id.splashFragment)
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/fragments/HomeFragment.kt
@@ -3,14 +3,20 @@ package com.example.myapplication.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentHomeBinding
 import com.example.myapplication.utils.adapter.TaskAdapter
 import com.example.myapplication.utils.model.ToDoData
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -20,8 +26,8 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-class HomeFragment : Fragment(), ToDoDialogFragment.OnDialogNextBtnClickListener,
-    TaskAdapter.TaskAdapterInterface {
+class HomeFragment : Fragment() ,ToDoDialogFragment.OnDialogNextBtnClickListener,
+    TaskAdapter.TaskAdapterInterface, NavigationView.OnNavigationItemSelectedListener {
 
     private val TAG = "HomeFragment"
     private lateinit var binding: FragmentHomeBinding
@@ -29,7 +35,7 @@ class HomeFragment : Fragment(), ToDoDialogFragment.OnDialogNextBtnClickListener
     private var frag: ToDoDialogFragment? = null
     private lateinit var auth: FirebaseAuth
     private lateinit var authId: String
-
+    private lateinit var drawerLayout: DrawerLayout
     private lateinit var taskAdapter: TaskAdapter
     private lateinit var toDoItemList: MutableList<ToDoData>
 
@@ -94,21 +100,22 @@ class HomeFragment : Fragment(), ToDoDialogFragment.OnDialogNextBtnClickListener
     }
 
     private fun init() {
-
         auth = FirebaseAuth.getInstance()
         authId = auth.currentUser!!.uid
-        database = Firebase.database("https://todo-kotlin-e26c0-default-rtdb.asia-southeast1.firebasedatabase.app/").reference.child("Tasks")
+        database = Firebase.database("https://todo-kotlin-e26c0-default-rtdb.asia-southeast1.firebasedatabase.app/")
+            .reference.child("Tasks")
             .child(authId)
 
+        toDoItemList = mutableListOf()  // Move the initialization here
 
         binding.mainRecyclerView.setHasFixedSize(true)
         binding.mainRecyclerView.layoutManager = LinearLayoutManager(context)
 
-        toDoItemList = mutableListOf()
         taskAdapter = TaskAdapter(toDoItemList)
         taskAdapter.setListener(this)
         binding.mainRecyclerView.adapter = taskAdapter
     }
+
 
     override fun saveTask(todoTask: String, todoEdit: TextInputEditText) {
 
@@ -161,5 +168,24 @@ class HomeFragment : Fragment(), ToDoDialogFragment.OnDialogNextBtnClickListener
             ToDoDialogFragment.TAG
         )
     }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.logout -> {
+                logoutUser()
+            }
+            // Add other cases for additional menu items if needed
 
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    private fun logoutUser() {
+        // Log out the user using FirebaseAuth
+        FirebaseAuth.getInstance().signOut()
+
+        // TODO: You may want to navigate the user to the login screen or perform other actions after logout
+
+        // Example: If you are using Navigation component, navigate to the login fragment
+
+    }
 }
